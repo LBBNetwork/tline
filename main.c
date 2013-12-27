a2e068c21a28282b3cb4c116b7fd8ae8b9397e42
neko2k <Support@beige-box.com>
2013-12-27 11:34:55 -0600

    New stuff
    Added line count to the main write loop

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  *
  */
 #ifdef _WIN32
-	#define _CRT_SECURE_NO_WARNINGS 4 //shut up cl386
+        #define _CRT_SECURE_NO_WARNINGS 4 //shut up cl386
 #endif
  
 #include <stdio.h>
@@ -15,32 +15,32 @@
 
 int main(int argc,char **argv)
 {
-	printf("thousand-line operspam\ncopyright (c) 2012 The Little Beige Box\nwww.beige-box.com\n\n");
+        printf("thousand-line operspam\ncopyright (c) 2012-2013 The Little Beige Box\nwww.beige-box.com\n\n");
 
-	if(argc != 4)
-	{
-		printf("usage: %s <text> <number of lines> <filename>\n",argv[0]);
-		return 0;
-	}
-	else
-	{
-		//int lines;
-		unsigned int numlines,lines;
-		char words[128];
+        if(argc != 4)
+        {
+                printf("usage: %s <text> <number of lines> <filename>\n",argv[0]);
+                return 0;
+        }
+        else
+        {
+                //int lines;
+                unsigned int numlines,lines;
+                char words[128];
+		FILE *filename;
 
-		lines = atoi(argv[2]);
+                lines = atoi(argv[2]);
+                sprintf(words,"%s\n",argv[1]);                
+                filename = fopen(argv[3],"a+");
 
-		sprintf(words,"%s\n",argv[1]);
-
-		FILE *file;
-		file = fopen(argv[3],"a+");
-
-		for(numlines = 0; numlines < lines; numlines++)
-		{
-			fprintf(file,words);
-		}
-		fclose(file);
-	}
-	printf("saved to %s\n",argv[3]);
-	return 0;
+                for(numlines = 0; numlines < lines; numlines++)
+                {
+			printf("writing line %d of %d... ",numlines, lines);
+                        fprintf(filename,words);
+			printf("done.\n");
+                }
+                fclose(filename);
+        }
+        printf("saved to %s\n",argv[3]);
+        return 0;
 }
